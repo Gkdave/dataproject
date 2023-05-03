8a00f740330d49b15a730802768b24a34be7a39a
Gajendra <davegk59@gmail.com>
2023-05-03 09:53:56 -0700

    first commit

---
+++ app4/views.py
@@ -0,0 +1,10 @@
+from django.contrib import auth
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
